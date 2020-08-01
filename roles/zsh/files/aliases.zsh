f98eddb3e55d3a8772f11e5ef49a57b5bc4e1388
Satish Viswanathan <satish.viswanathan.in@gmail.com>
2020-08-01 21:48:57 +0530

    Churned around the initial files

--- roles/zsh/files/aliases.zsh
+++ roles/zsh/files/aliases.zsh
@@ -3,6 +3,17 @@
 
 alias open="xdg-open"
 alias vim="nvim"
+alias x='exit'
+alias c='clear'
+alias cls='printf "\033c"'
+
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+
+alias rr='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
+alias h='history'
+alias hh='history | fzf'
 
 # END: Aliases
 ###############################################################################
